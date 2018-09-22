165acab88c0582b33ac6711db01f036ab3382092
Shuang <hss12103@gmail.com>
2018-09-22 22:21:20 +0800

    Test

---
+++ assets/fonts/includes/header-ch.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="zh-Hans">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	
+	<!-- Bootstrap core CSS -->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Plugins -->
+	<link href="assets/css/simpletextrotator.css" rel="stylesheet">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href="assets/css/et-line-font.css" rel="stylesheet">
+	<link href="assets/css/magnific-popup.css" rel="stylesheet">
+	<link href="assets/css/flexslider.css" rel="stylesheet">
+	<link href="assets/css/animate.css" rel="stylesheet">
+	
+	<!-- Template core CSS -->
+	<link href="assets/css/style.css" rel="stylesheet">
+	
+	<!-- Custom css -->
+	<link href="assets/css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+	<!-- PRELOADER -->
+	<div class="page-loader">
+		<div class="loader">Loading...</div>
+	</div>
+	<!-- /PRELOADER -->
+
+	<!-- NAVIGATION -->
+	<nav class="navbar navbar-custom navbar-transparent navbar-fixed-top" role="navigation">
+
+			<div class="container">
+		
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#custom-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index">霍霜霜作品集</a>
+				</div>
+		
+				<div class="collapse navbar-collapse" id="custom-collapse">
+		
+					<ul class="nav navbar-nav navbar-right">
+		
+						<li><a href="index">主页</a></li>
+
+						<li><a href="resume">简历</a></li>
+		
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">作品集</a>
+							<ul class="dropdown-menu" role="menu">
+		
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">UI/UX设计</a>
+									<ul class="dropdown-menu">
+										<li><a href="donatenyc">donateNYC App</a></li>
+										<li><a href="dsnyapp">DSNY App<br/>UI动画</a></li>
+										<li><a href="smart">SMART系统</a></li>
+									</ul>
+								</li>
+		
+
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Development</a>
+									<ul class="dropdown-menu">
+										<li><a href="junwang">王钧律师事务所官网</a></li>
+						
+									</ul>
+								</li>
+		
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">视觉</a>
+									<ul class="dropdown-menu">
+										<li><a href="pov_infographics">POV纪录片信息图</a></li>
+										<li><a href="celebrity">名人的一生有多长</a></li>
+									</ul>
+								</li>
+
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">新媒体艺术</a>
+									<ul class="dropdown-menu">
+										<li><a href="invisible">看不见的界线</a></li>
+										<li><a href="allinpieces">尽皆碎片</a></li>
+						
+									</ul>
+								</li>
+							</ul>
+						</li>
+		
+						<li><a href="contact">联系方式</a></li>
+		
+					</ul>
+				</div>
+		
+			</div>
+
+	</nav>
+	<!-- /NAVIGATION -->
---
+++ assets/fonts/includes/header.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<title>Shuangshuang Portfolio</title>
+	
+	<!-- Bootstrap core CSS -->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Plugins -->
+	<link href="assets/css/simpletextrotator.css" rel="stylesheet">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href="assets/css/et-line-font.css" rel="stylesheet">
+	<link href="assets/css/magnific-popup.css" rel="stylesheet">
+	<link href="assets/css/flexslider.css" rel="stylesheet">
+	<link href="assets/css/animate.css" rel="stylesheet">
+	
+	<!-- Template core CSS -->
+	<link href="assets/css/style.css" rel="stylesheet">
+	
+	<!-- Custom css -->
+	<link href="assets/css/custom.css" rel="stylesheet">
+
+	<!-- Javascript files -->
+	<script src="assets/js/jquery-2.1.3.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+	<script src="assets/js/appear.js"></script>
+	<script src="assets/js/jquery.simple-text-rotator.min.js"></script>
+	<script src="assets/js/jqBootstrapValidation.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<script src="assets/js/gmaps.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.js"></script>
+	<script src="assets/js/jquery.flexslider-min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/jquery.fitvids.js"></script>
+	<script src="assets/js/smoothscroll.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/contact.js"></script>
+	<script src="assets/js/processing.min.js"></script> 
+	<script src="assets/js/custom.js"></script>
+
+</head>
+<body>
+
+	<!-- PRELOADER -->
+	<div class="page-loader">
+		<div class="loader">Loading...</div>
+	</div>
+	<!-- /PRELOADER -->
+
+	<!-- NAVIGATION -->
+	<nav class="navbar navbar-custom navbar-transparent navbar-fixed-top" role="navigation">
+
+			<div class="container">
+		
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#custom-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index">Shuangshuang</a>
+				</div>
+		
+				<div class="collapse navbar-collapse" id="custom-collapse">
+		
+					<ul class="nav navbar-nav navbar-right">
+		
+						<li><a href="index">Home</a></li>
+
+						<li><a href="resume">Resume</a></li>
+		
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio</a>
+							<ul class="dropdown-menu" role="menu">
+		
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">UI/UX Design</a>
+									<ul class="dropdown-menu">
+										<li><a href="craigsbroker">Craigsbroker NYC</a></li>
+										<li><a href="smart">SMART</a></li>
+										<li><a href="donatenyc">donateNYC App</a></li>
+										<li><a href="dsnyapp">DSNY App<br/>UI Animation</a></li>
+									</ul>
+								</li>
+								
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Development</a>
+									<ul class="dropdown-menu">
+										<li><a href="junwang">Jun Wang Law Offices</a></li>
+						
+									</ul>
+								</li>
+		
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Visual</a>
+									<ul class="dropdown-menu">
+										<li><a href="pov_infographics">POV Infographics</a></li>
+										<li><a href="celebrity">A Celebrity's Lifetime</a></li>
+						
+									</ul>
+								</li>
+
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">New Media Art</a>
+									<ul class="dropdown-menu">
+										<li><a href="invisible">Invisible Territories</a></li>
+										<li><a href="allinpieces">All in Pieces</a></li>
+						
+									</ul>
+								</li>
+		
+							</ul>
+						</li>
+						
+						<li><a href="archive">Archive</a></li>
+						<li><a href="contact">Contact</a></li>
+		
+					</ul>
+				</div>
+		
+			</div>
+
+	</nav>
